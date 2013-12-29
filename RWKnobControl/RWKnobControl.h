c63bf01db8de2efb67056d95a95c17b0ff0724ae
Sam Davies <sdavies@shinobicontrols.com>
2013-12-29 21:27:21 +0000

    Create a static library for the knob control
    Literally just added it. Requires a little tidying up now.

---
+++ RWKnobControl/RWKnobControl.h
@@ -0,0 +1,13 @@
+//
+//  RWKnobControl.h
+//  RWKnobControl
+//
+//  Created by Sam Davies on 29/12/2013.
+//  Copyright (c) 2013 RayWenderlich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RWKnobControl : NSObject
+
+@end
---
+++ RWKnobControl/RWKnobControl.m
@@ -0,0 +1,13 @@
+//
+//  RWKnobControl.m
+//  RWKnobControl
+//
+//  Created by Sam Davies on 29/12/2013.
+//  Copyright (c) 2013 RayWenderlich. All rights reserved.
+//
+
+#import "RWKnobControl.h"
+
+@implementation RWKnobControl
+
+@end
